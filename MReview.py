c559b6caf21c4ed729b49f5ecc43919f443a7de2
Mousa Alhaddad <mousa.j.alhaddad@gmail.com>
2019-04-08 16:26:01 +0300

    new: uploading a python file
    The script is simple, and it depends on pandas. 
    The sliceQuestions function returns a slicing list from the original data set indexes 
    based on multiple variables. 
    
    v1.0

---
+++ MReview.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+file = 'Clinical Questions.csv'
+
+
+def importDataFrame(file):
+    if file.split('.')[1] == 'csv':
+        return pd.read_csv(file)
+    else:
+        return pd.ExcelFile(file).parse(0)
+
+
+def sliceQuestions(frame, Exclude = [], Specialty= 'All', Diagnosis = 'All',
+                     Emergency = 'All', Pharmacology = 'All'):
+     # Date = 'All' 
+     if Specialty != 'All': 
+         frame = frame[frame.Specialty.isin(Specialty.split(','))]
+     if Diagnosis != 'All':
+         frame = frame[frame.Diagnosis.isin(Specialty.split(','))]
+     if Emergency != 'All':
+         if Emergency == True: 
+             frame = frame[frame.Emergency == 'Yes']
+         elif Emergency == False:
+             frame = frame[frame.Emergency == 'No']
+     if Pharmacology != 'All':
+         if Pharmacology == True: 
+             frame = frame[frame.Pharmacology == 'Yes']
+         elif Pharmacology == False:
+             frame = frame[frame.Pharmacology == 'No']
+     
+     questions = [x for x in list(frame.index) if x not in Exclude]    
+     return (questions)
+     
+     
+
+
+
+
+
