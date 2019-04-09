f4fde289148d5e95bfa7ffbee3cbeea36bd7cda0
Mousa Alhaddad <mousa.j.alhaddad@gmail.com>
2019-04-09 09:44:35 +0300

    feature: adding a function: descriptiveStatistics
    I added a function by the name descriptiveStatistics to describe the data of 
    a data frame with medical questions. 
    
    v3

--- MReview.py
+++ MReview.py
@@ -2,13 +2,11 @@ import pandas as pd
 
 file = 'Clinical Questions.csv'
 
-
 def importDataFrame(file):
     if file.split('.')[1] == 'csv':
         return pd.read_csv(file,parse_dates=['Date'])
     else:
         return pd.ExcelFile(file).parse(0,parse_dates=['Date'])
-
 
 def sliceQuestions(frame, Exclude = [], Specialty= 'All', Diagnosis = 'All',
                      Emergency = 'All', Pharmacology = 'All'):
@@ -31,8 +29,16 @@ def sliceQuestions(frame, Exclude = [], Specialty= 'All', Diagnosis = 'All',
      questions = [x for x in list(frame.index) if x not in Exclude]    
      return (questions)
      
-     
-
+def descriptiveStatistics(frame):
+    print('Our database has {} questions.'.format(len(frame)))
+    print('The questions cover {} different specialties.'.format(len(frame.Specialty.value_counts())),end='\n\n')
+    print('Here are the numbers of questions of the top ten specialties:')
+    for x in frame.Specialty.value_counts()[0:10].index:
+        print ("    ",x,": ",frame.Specialty.value_counts().loc[x], 
+               " ({} Emergency".format(len(frame[(frame.Specialty==x)&(frame.Emergency=='Yes')])),
+               " and {} Pharmacology)".format(len(frame[(frame.Specialty==x)&(frame.Pharmacology=='Yes')])), sep='')
+    print('\nThe questions were collected over {} days since {}.'.format((frame.Date.iloc[-1]-frame.Date.iloc[0]).days+1,
+                                frame.Date.iloc[-1].strftime('%Y-%m-%d')))
 
 
 
